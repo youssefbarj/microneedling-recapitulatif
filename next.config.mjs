970a9182390b307a1587e80d3a6b4fa70119b274
youssefbarj <151462053+youssefbarj@users.noreply.github.com>
2025-08-11 20:15:59 +0100

    Add files via upload

--- next.config.mjs
+++ next.config.mjs
@@ -9,6 +9,23 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'X-Frame-Options',
+            value: 'SAMEORIGIN',
+          },
+          {
+            key: 'Content-Security-Policy',
+            value: "frame-ancestors 'self' *;",
+          },
+        ],
+      },
+    ]
+  },
 }
 
 export default nextConfig
