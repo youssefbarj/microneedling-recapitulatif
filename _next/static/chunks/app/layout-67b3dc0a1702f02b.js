15971e10841779d1574c1fa2230249cedef811b1
youssefbarj <youssefbarj@users.noreply.github.com>
2026-05-12 16:03:38 +0000

    deploy: 40ef6ab8512a5cfd0569dd2df70515192b7f4fa5

---
+++ _next/static/chunks/app/layout-67b3dc0a1702f02b.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[177],{711:(e,s,l)=>{Promise.resolve().then(l.t.bind(l,1927,23)),Promise.resolve().then(l.t.bind(l,814,23))},814:()=>{},1927:e=>{e.exports={style:{fontFamily:"'Inter', 'Inter Fallback'",fontStyle:"normal"},className:"__className_f367f3"}}},e=>{var s=s=>e(e.s=s);e.O(0,[791,22,575,358],()=>s(711)),_N_E=e.O()}]);
